ed095c76da888a41a75fba613af68c561b188cc5
choi-il-woo <chldlfdn0440@naver.com>
2026-03-08 16:27:18 +0900

    feat: 라우터 설정 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes></Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
