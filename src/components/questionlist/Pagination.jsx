13b71f6e7b697ef00a363c36c515afd08d958a1d
chaewon <devchae10@gmail.com>
2026-03-09 17:35:35 +0900

    design: 질문목록 페이지 UI 작업, 공통 컴포넌트 파일 코드 위치 변경

--- src/components/questionlist/Pagination.jsx
+++ src/components/questionlist/Pagination.jsx
@@ -1,5 +1,7 @@
 import styled, { css } from 'styled-components';
 
+const MOBILE = '768px';
+
 const SIZE = {
   large: css`
     --box: 40px;
@@ -14,12 +16,60 @@ const SIZE = {
 const PAGES = [1, 2, 3, 4, 5];
 const ACTIVE_PAGE = 4;
 
+function Pagination({ size = 'large', responsive = false }) {
+  return (
+    <PaginationWrap
+      $size={size}
+      $responsive={responsive}
+      aria-label="페이지네이션"
+    >
+      <PageNavArrowBox>
+        <PageNavArrow type="button" aria-label="이전 페이지">
+          {'<'}
+        </PageNavArrow>
+      </PageNavArrowBox>
+
+      <PageNavList>
+        {PAGES.map((page) => {
+          const isActive = page === ACTIVE_PAGE;
+
+          return (
+            <PageNavItem key={page}>
+              <PageNavLink
+                type="button"
+                $active={isActive}
+                aria-current={isActive ? 'page' : undefined}
+              >
+                {page}
+              </PageNavLink>
+            </PageNavItem>
+          );
+        })}
+      </PageNavList>
+
+      <PageNavArrowBox>
+        <PageNavArrow type="button" aria-label="다음 페이지">
+          {'>'}
+        </PageNavArrow>
+      </PageNavArrowBox>
+    </PaginationWrap>
+  );
+}
+
 const PaginationWrap = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
 
   ${({ $size }) => SIZE[$size] ?? SIZE.large}
+
+  ${({ $responsive }) =>
+    $responsive &&
+    css`
+      @media (max-width: ${MOBILE}) {
+        ${SIZE.small}
+      }
+    `}
 `;
 
 const PageNavArrowBox = styled.div`
@@ -54,8 +104,9 @@ const PageNavItem = styled.li`
 `;
 
 const PageNavLink = styled.button`
-  width: 100%;
-  height: 100%;
+  padding: 3px 6px;
+  width: auto;
+  height: auto;
 
   font-size: var(--font);
   font-family: Actor, sans-serif;
@@ -65,41 +116,5 @@ const PageNavLink = styled.button`
 
   font-weight: ${({ $active }) => ($active ? 500 : 400)};
 `;
-
-function Pagination({ size = 'large' }) {
-  return (
-    <PaginationWrap $size={size} aria-label="페이지네이션">
-      <PageNavArrowBox>
-        <PageNavArrow type="button" aria-label="이전 페이지">
-          {'<'}
-        </PageNavArrow>
-      </PageNavArrowBox>
-
-      <PageNavList>
-        {PAGES.map((page) => {
-          const isActive = page === ACTIVE_PAGE;
-
-          return (
-            <PageNavItem key={page}>
-              <PageNavLink
-                type="button"
-                $active={isActive}
-                aria-current={isActive ? 'page' : undefined}
-              >
-                {page}
-              </PageNavLink>
-            </PageNavItem>
-          );
-        })}
-      </PageNavList>
-
-      <PageNavArrowBox>
-        <PageNavArrow type="button" aria-label="다음 페이지">
-          {'>'}
-        </PageNavArrow>
-      </PageNavArrowBox>
-    </PaginationWrap>
-  );
-}
 
 export default Pagination;
