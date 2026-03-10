1e2706ece3e9027d629f22031045d2ef62b45562
hhhnseo <wkdgustj3014@naver.com>
2026-03-10 15:03:58 +0900

    App.jsx Edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import './index.css';
-import AnswerPage from './pages/AnswerPage';
+
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<AnswerPage />} />
+      
     </Routes>
   )
 
