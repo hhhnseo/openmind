06ee3969e1e2668c61b1c5ee58c8d6a9df7e9e9b
choi-il-woo <chldlfdn0440@naver.com>
2026-03-13 13:52:18 +0900

    feat: 챗봇 구현 및 연동

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,19 @@ import Home from './pages/Home';
 import QuestionList from './pages/QuestionList';
 import Ranking from './pages/Ranking';
 import Answer from './pages/Answer';
+import Chatbot from './components/chatbot/Chatbot';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/list" element={<QuestionList />} />
-      <Route path="/ranking" element={<Ranking />} />
-      <Route path="/post/:id/answer" element={<Answer />} />
-    </Routes>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/list" element={<QuestionList />} />
+        <Route path="/ranking" element={<Ranking />} />
+        <Route path="/post/:id/answer" element={<Answer />} />
+      </Routes>
+      <Chatbot />
+    </div>
   );
 }
 
